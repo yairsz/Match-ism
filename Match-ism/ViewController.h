f0fbc644c0b06a891f58930c6f5d03071583d7d4
Yair Szarf <yaryo@hotmail.com>
2013-02-07 00:31:40 -0430

    Initial Commit

---
+++ Match-ism/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Match-ism
+//
+//  Created by Yair Szarf on 2/7/13.
+//  Copyright (c) 2013 Yair Szarf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Match-ism/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Match-ism
+//
+//  Created by Yair Szarf on 2/7/13.
+//  Copyright (c) 2013 Yair Szarf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
